cf73e8c01d4c000fa2e508499916cafd1fda7554
wms1996 <1570336197@qq.com>
2020-10-07 16:44:49 +0800

    初始化了后台初始页面--渲染头像

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,20 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的ajax请求之前，同意拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    // 统一为有权限的接口设置 headers 请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空 token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登录页面
+            location.href = '/login.html';
+        }
+    }
 })
